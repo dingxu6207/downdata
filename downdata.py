936f9906d473ee2d2b8296d4171deb26d4218123
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2021-03-16 14:55:21 +0800

    星明数据下载

--- downdata.py
+++ downdata.py
@@ -12,7 +12,7 @@ import sys
 
 downroute = sys.argv[1]
 
-#downroute = 'http://psp.china-vo.org/pspdata/2019/11/20191104/M31-18object/'
+#downroute = 'https://nadc.china-vo.org/psp/csp/2020CSP/20201223/CSP-VAR-0645/'
 # open the url and read
 def getHtml(url):
     page = urllib.request.urlopen(url)
@@ -23,23 +23,25 @@ def getHtml(url):
 # compile the regular expressions and find
 # all stuff we need
 def getUrl(html):
-    reg = r'(?:href|HREF)="?((?:http://)?.+?\.fts)'
+    #reg = r'(?:href|HREF)="?((?:http://)?.+?\.fts)'
+    reg = r'(?:href|HREF)="?((?:https://)?.+?\.fts)'
     url_re = re.compile(reg)
     url_lst = url_re.findall(html.decode('gb2312'))
     return(url_lst)
 
 def getFile(url):
     file_name = url.split('/')[-1]
-    print(file_name)
+    #print(file_name)
     u = urllib.request.urlopen(url)
+    #print(u.read()) 
+    
     f = open(file_name, 'wb')
-
+    #print(u)
     block_sz = 8192
     while True:
         buffer = u.read(block_sz)
         if not buffer:
             break
-
         f.write(buffer)
     f.close()
     print ("Sucessful to download" + " " + file_name)
@@ -50,7 +52,7 @@ root_url = raw_url
 html = getHtml(raw_url)
 url_lst = getUrl(html)
 
-#downpath = 'E:/shunbianyuan/data/'
+#downpath = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\20201223\\CSP-VAR-0645\\'
 downpath = sys.argv[2]
 os.chdir(downpath)
 
--- downdatacmd.py
+++ downdatacmd.py
@@ -5,34 +5,39 @@ Created on Fri Oct  4 00:17:16 2019
 @author: dingxu
 """
 import os
-listroute = [0 for i in range(10)]
+
+'''
+listroute = [0 for i in range(22)]
 #listroute[0] = 'https://nadc.china-vo.org/psp/csp/2020CSP/20201122/AutoFlat20201122/'
-listpath = [0 for i in range(10)]
+listpath = [0 for i in range(22)]
 #listpath[0] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\20201122\\flat20201122\\'
   
-for i in range(10):
-    listroute[i] = 'https://nadc.china-vo.org/psp/csp/2020CSP/202011'+str(22+i)+'/AutoFlat202011'+str(22+i)+'/'
+for i in range(22):
+    listroute[i] = 'https://nadc.china-vo.org/psp/csp/2020CSP/202012'+str(10+i)+'/AutoFlat202012'+str(10+i)+'/'
     
-for i in range(10):
-    listpath[i] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\202011'+str(22+i)+'\\flat202011'+str(22+i)+'\\'
+for i in range(22):
+    listpath[i] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\202012'+str(10+i)+'\\flat202012'+str(10+i)+'\\'
     
-for i in range(10):
+for i in range(22):
     cmd = 'python downdata.py'+' '+listroute[i]+' '+listpath[i]
     os.system(cmd)
+'''    
     
     
-    
-dlistroute = [0 for i in range(10)]
+dlistroute = [0 for i in range(22)]
 #listroute[0] = 'https://nadc.china-vo.org/psp/csp/2020CSP/20201122/AutoFlat20201122/'
-dlistpath = [0 for i in range(10)]
-#listpath[0] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\20201122\\flat20201122\\'
-  
-for i in range(10):
-    dlistroute[i] = 'https://nadc.china-vo.org/psp/csp/2020CSP/202011'+str(22+i)+'/CSP-VAR-0645'+'/'
+#dlistroute[0] = 'https://nadc.china-vo.org/psp/csp/2020CSP/20201223/CSP-VAR-0645/'
+dlistpath = [0 for i in range(22)]
+#dlistpath[0] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\20201223\\CSP-VAR-0645\\'
+ 
+
+for i in range(22):
+    dlistroute[i] = 'https://nadc.china-vo.org/psp/csp/2020CSP/202012'+str(22+i)+'/CSP-VAR-0645'+'/'
     
-for i in range(10):
-    dlistpath[i] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\202011'+str(22+i)+'\\CSP-VAR-0645'+'\\'
-    
-for i in range(10):
+for i in range(22):
+    dlistpath[i] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\202012'+str(22+i)+'\\CSP-VAR-0645'+'\\'
+
+   
+for i in range(12):
     cmd = 'python downdata.py'+' '+dlistroute[i]+' '+dlistpath[i]
     os.system(cmd)
