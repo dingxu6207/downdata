b93ce0d66a731d0d9dc43d35b408edf878255088
dingxu6207 <dx409@qq.com>
2021-06-09 10:46:45 +0800

    bias下载

--- biasdarkdown.py
+++ biasdarkdown.py
@@ -9,16 +9,17 @@ import os
 
 
 listroute = [0 for i in range(22)]
+#https://nadc.china-vo.org/psp/csp/calibration/dark/-35c/20200204/
 #listroute[0] = 'https://nadc.china-vo.org/psp/csp/2020CSP/20201122/AutoFlat20201122/'
-listroute[0] = 'https://nadc.china-vo.org/psp/csp/calibration/bias/-40c/20201224/'
-listroute[1] = 'https://nadc.china-vo.org/psp/csp/calibration/bias/-40c/20201202/'
-listroute[2] = 'https://nadc.china-vo.org/psp/csp/calibration/bias/-35c/20200204/'
+listroute[0] = 'https://nadc.china-vo.org/psp/csp/calibration/dark/-40c/20201224/'
+listroute[1] = 'https://nadc.china-vo.org/psp/csp/calibration/dark/-40c/20201202/'
+listroute[2] = 'https://nadc.china-vo.org/psp/csp/calibration/dark/-35c/20200204/'
 
 listpath = [0 for i in range(22)]
 #listpath[0] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\20201122\\flat20201122\\'
-listpath[0] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\calibration\\bias\\-40c\\20201224\\'
-listpath[1] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\calibration\\bias\\-40c\\20201202\\'  
-listpath[2] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\calibration\\bias\\-35c\\20200204\\' 
+listpath[0] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\calibration\\dark\\-40c\\20201224\\'
+listpath[1] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\calibration\\dark\\-40c\\20201202\\'  
+listpath[2] = 'E:\\shunbianyuan\\data\\xingmingdata\\downdata\\calibration\\dark\\-35c\\20200204\\' 
 
 #for i in range(22):
 #    listroute[i] = 'https://nadc.china-vo.org/psp/csp/2020CSP/202012'+str(10+i)+'/AutoFlat202012'+str(10+i)+'/'
